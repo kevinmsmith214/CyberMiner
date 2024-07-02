d94ed4357e7092c2ff7367a491fdc999a035997d
kevinmsmith13 <kevinmsmith13@tamu.edu>
2024-07-01 19:32:04 -0500

    json formatting fix

--- src/Crawler/Crawler/pipelines.py
+++ src/Crawler/Crawler/pipelines.py
@@ -7,19 +7,20 @@
 # useful for handling different item types with a single interface
 from itemadapter import ItemAdapter
 import json
+import os
 
 
 class CrawlerPipeline:
 
     def open_spider(self, spider):
-        self.file = open('Output/scraped_data.json', 'a')  # Open file in append mode
-        self.file.write("[\n")  # Start with an opening bracket for JSON array
-
+        self.file_path = 'Output/scraped_data.json'
+        if os.path.exists(self.file_path):
+            os.remove(self.file_path)  # Remove the file if it exists
+        self.file = open(self.file_path, 'a')
     def process_item(self, item, spider):
-        line = json.dumps(dict(item)) + "\n"
+        line = json.dumps(dict(item)) + ",\n"
         self.file.write(line)
         return item
     
     def close_spider(self, spider):
-        self.file.write("]\n")
         self.file.close()
